0d07a3f5243b63d8598184f0ad07796fac95ab7a
Jayraj Rajesh Deshmukh <jairaj12354@gmail.com>
2026-07-10 22:23:23 +0530

    Time: 1 ms (42.26%), Space: 19.2 MB (99.17%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,23 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         arr=[]
-        def f(n,i,s):
-            if i==2*n+1:
-                r=0
-                l=0
-                for j in s:
-                    if j=="(":
-                        r+=1
-                    if j==")":
-                        l+=1
-                    if l>r:
-                        return 
-                if l==r:
-                    arr.append(s)
+
+        def f(n,i,s,l,r):
+            if i==2*n and l==n and r==l:
+                arr.append("".join(s))
                 return 
-            f(n,i+1,s+"(")
-            f(n,i+1,s+")")
-        f(n,1,"")
+            if r<n:
+                s.append("(")
+                f(n,i+1,s,l,r+1)
+                s.pop()
+            if l<r:
+                s.append(")")
+                f(n,i+1,s,l+1,r)
+                s.pop()
+        f(n,0,[],0,0)
         return arr
             
 
